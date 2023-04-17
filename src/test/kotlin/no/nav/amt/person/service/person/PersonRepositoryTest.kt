6f543bfe7b8235166ddb96e6ac678508646ebb54
Tormod Fossum <tormod.fossum@nav.no>
2023-04-17 13:49:57 +0200

    Rydd opp database etter testene og fiks clock skew i tester

--- src/test/kotlin/no/nav/amt/person/service/person/PersonRepositoryTest.kt
+++ src/test/kotlin/no/nav/amt/person/service/person/PersonRepositoryTest.kt
@@ -3,17 +3,29 @@ package no.nav.amt.person.service.person
 import io.kotest.matchers.shouldBe
 import no.nav.amt.person.service.data.TestData
 import no.nav.amt.person.service.data.TestDataRepository
+import no.nav.amt.person.service.utils.DbTestDataUtils
 import no.nav.amt.person.service.utils.SingletonPostgresContainer
+import no.nav.amt.person.service.utils.shouldBeEqualTo
+import org.junit.AfterClass
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import java.util.UUID
 
 class PersonRepositoryTest {
-	val dataSource = SingletonPostgresContainer.getDataSource()
-	val jdbcTemplate = NamedParameterJdbcTemplate(dataSource)
-	val testRepository = TestDataRepository(jdbcTemplate)
-	val repository = PersonRepository(jdbcTemplate)
+
+	companion object {
+		val dataSource = SingletonPostgresContainer.getDataSource()
+		val jdbcTemplate = NamedParameterJdbcTemplate(dataSource)
+		val testRepository = TestDataRepository(jdbcTemplate)
+		val repository = PersonRepository(jdbcTemplate)
+
+		@JvmStatic
+		@AfterClass
+		fun tearDown() {
+			DbTestDataUtils.cleanDatabase(dataSource)
+		}
+	}
 
 	@Test
 	fun `get - person finnes - returnerer person`() {
@@ -29,8 +41,8 @@ class PersonRepositoryTest {
 		faktiskPerson.fornavn shouldBe person.fornavn
 		faktiskPerson.mellomnavn shouldBe person.mellomnavn
 		faktiskPerson.etternavn shouldBe person.etternavn
-		faktiskPerson.createdAt shouldBe person.createdAt
-		faktiskPerson.modifiedAt shouldBe person.modifiedAt
+		faktiskPerson.createdAt shouldBeEqualTo person.createdAt
+		faktiskPerson.modifiedAt shouldBeEqualTo person.modifiedAt
 	}
 
 	@Test
