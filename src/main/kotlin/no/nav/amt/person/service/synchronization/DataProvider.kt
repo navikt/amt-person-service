04acf0a8930e2a2c6a69ab0541403ed5a789e4da
Izis Frøyhov <izis.froyhov@nav.no>
2023-08-29 14:29:09 +0200

    Sletter ubrukte filer

--- src/main/kotlin/no/nav/amt/person/service/synchronization/DataProvider.kt
+++
@@ -1,5 +0,0 @@
-package no.nav.amt.person.service.synchronization
-
-enum class DataProvider {
-	KRR
-}
--- src/main/kotlin/no/nav/amt/person/service/synchronization/SynchronizationUpsert.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.amt.person.service.synchronization
-
-import java.util.*
-
-data class SynchronizationUpsert (
-	val dataProvider: DataProvider,
-	val tableName: String,
-	val rowId: UUID,
-)
