db4c930bb0dbd7d9be58f806955e6daab4b33573
Tormod Fossum <tormod.fossum@nav.no>
2024-05-28 11:53:50 +0200

    Legg til postgres support for flyway

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
 	implementation("com.squareup.okhttp3:okhttp:$okhttp3Version")
 	implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
 	implementation("org.postgresql:postgresql")
 
 	implementation("io.micrometer:micrometer-registry-prometheus")
