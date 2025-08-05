32be348dc21510576c5e49817627a685a8cdd9a5
Roar Skinderviken <roar.skinderviken@nav.no>
2025-08-05 21:19:13 +0200

    Forenklet RepositoryTestBase

--- src/test/kotlin/no/nav/amt/person/service/data/RepositoryTestBase.kt
+++ src/test/kotlin/no/nav/amt/person/service/data/RepositoryTestBase.kt
@@ -4,11 +4,10 @@ import no.nav.amt.person.service.utils.DbTestDataUtils.cleanDatabase
 import org.junit.jupiter.api.AfterEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureJdbc
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.context.annotation.Import
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.context.TestConstructor
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.containers.wait.strategy.Wait
@@ -35,22 +34,9 @@ abstract class RepositoryTestBase {
 	companion object {
 		private const val POSTGRES_DOCKER_IMAGE_NAME = "postgres:14-alpine"
 
+		@ServiceConnection
 		@Suppress("unused")
 		private val postgres =
-			createContainer().apply {
-				start()
-			}
-
-		@JvmStatic
-		@DynamicPropertySource
-		@Suppress("unused")
-		fun overrideProps(registry: DynamicPropertyRegistry) {
-			registry.add("spring.datasource.url", postgres::getJdbcUrl)
-			registry.add("spring.datasource.username", postgres::getUsername)
-			registry.add("spring.datasource.password", postgres::getPassword)
-		}
-
-		private fun createContainer(): PostgreSQLContainer<Nothing> =
 			PostgreSQLContainer<Nothing>(
 				DockerImageName
 					.parse(POSTGRES_DOCKER_IMAGE_NAME)
