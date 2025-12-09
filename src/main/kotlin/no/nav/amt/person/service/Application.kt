53825b99d37853b020b3f04b940c394f047281fa
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-12-09 16:13:27 +0100

    Fjernet duplikat: @EnableResilientMethods

--- src/main/kotlin/no/nav/amt/person/service/Application.kt
+++ src/main/kotlin/no/nav/amt/person/service/Application.kt
@@ -2,9 +2,7 @@ package no.nav.amt.person.service
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.resilience.annotation.EnableResilientMethods
 
-@EnableResilientMethods
 @SpringBootApplication
 class Application
 
