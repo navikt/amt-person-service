e80a95599c4f0a5e43f4093ec58b347e3d8d8a6c
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-02-17 16:01:53 +0100

    Lagt til null-sjekk i SkjermetPersonConsumer

--- src/main/kotlin/no/nav/amt/person/service/kafka/consumer/SkjermetPersonConsumer.kt
+++ src/main/kotlin/no/nav/amt/person/service/kafka/consumer/SkjermetPersonConsumer.kt
@@ -2,6 +2,7 @@ package no.nav.amt.person.service.kafka.consumer
 
 import no.nav.amt.person.service.navbruker.NavBrukerRepository
 import no.nav.amt.person.service.navbruker.NavBrukerService
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import tools.jackson.databind.ObjectMapper
 import tools.jackson.module.kotlin.readValue
@@ -12,10 +13,17 @@ class SkjermetPersonConsumer(
 	private val navBrukerService: NavBrukerService,
 	private val objectMapper: ObjectMapper,
 ) {
+	private val log = LoggerFactory.getLogger(javaClass)
+
 	fun ingest(
 		key: String,
-		value: String,
+		value: String?,
 	) {
+		if (value == null) {
+			log.warn("Mottok uventet tombstone for Kafka-record.")
+			return
+		}
+
 		val erSkjermet = objectMapper.readValue<Boolean>(value)
 
 		val brukerId = navBrukerRepository.finnBrukerId(key) ?: return
