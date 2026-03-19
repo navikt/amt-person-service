bdc6bf7c5d14bbb0720f1cd397503d89a3822ce8
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-03-19 17:03:19 +0100

    Spring Boot 4.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     val kotlinVersion = libs.versions.kotlin.version
 
     kotlin("jvm")
-    id("org.springframework.boot") version "4.0.3"
+    id("org.springframework.boot") version "4.0.4"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("plugin.serialization") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
