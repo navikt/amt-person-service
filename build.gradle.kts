9cf9b811d2b35eee2559020e4207812030a73498
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-04-19 14:45:37 +0200

    Flyttet versjoner inn i libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,10 @@
 plugins {
-    val kotlinVersion = libs.versions.kotlin.version
-
     kotlin("jvm")
-    id("org.springframework.boot") version "4.0.5"
-    id("io.spring.dependency-management") version "1.1.7"
-    kotlin("plugin.serialization") version kotlinVersion
-    kotlin("plugin.spring") version kotlinVersion
-    id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.ktlint)
 }
-
-group = "no.nav.amt-person-service"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
     mavenCentral()
@@ -19,20 +12,13 @@ repositories {
     maven { url = uri("https://packages.confluent.io/maven/") }
 }
 
-val commonVersion = "3.2026.04.08_08.37-229807cc181a"
-val okhttp3Version = "5.3.2"
-val kotestVersion = "6.1.11"
-val poaoTilgangVersion = "2025.11.03_13.40-18456d0598be"
-val tokenSupportVersion = "6.0.5"
-val mockkVersion = "1.14.9"
-val lang3Version = "3.20.0"
-val shedlockVersion = "7.7.0"
-val confluentVersion = "8.1.1"
-val mockOauth2ServerVersion = "3.0.1"
-val logstashEncoderVersion = "9.0"
-val ktLintVersion = "1.8.0"
-val jacksonModuleKotlinVersion = "3.1.1"
-val amtLibVersion = "1.2026.03.01_23.16-831b1856e93b"
+dependencyManagement {
+    imports {
+        mavenBom(libs.tools.jackson.bom.get().toString())
+    }
+}
+
+extra["tomcat.version"] = libs.versions.tomcat.version.get()
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
@@ -42,27 +28,28 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-flyway")
 
-    implementation("tools.jackson.module:jackson-module-kotlin:$jacksonModuleKotlinVersion")
-    implementation("com.squareup.okhttp3:okhttp:$okhttp3Version")
+    implementation(libs.tools.jackson.module.kotlin)
+    implementation(libs.okhttp)
     implementation("org.flywaydb:flyway-database-postgresql")
     implementation("org.postgresql:postgresql")
 
     implementation("io.micrometer:micrometer-registry-prometheus")
-    implementation("no.nav.common:log:$commonVersion")
-    implementation("no.nav.common:token-client:$commonVersion")
-    implementation("no.nav.common:rest:$commonVersion")
-    implementation("no.nav.common:job:$commonVersion")
-    implementation("no.nav.common:kafka:$commonVersion")
 
-    implementation("net.logstash.logback:logstash-logback-encoder:$logstashEncoderVersion")
-    implementation("io.confluent:kafka-avro-serializer:$confluentVersion")
+    implementation(libs.nav.common.log)
+    implementation(libs.nav.common.token.client)
+    implementation(libs.nav.common.rest)
+    implementation(libs.nav.common.job)
+    implementation(libs.nav.common.kafka)
 
-    implementation("no.nav.poao-tilgang:client:$poaoTilgangVersion")
+    implementation(libs.logstash.encoder)
+    implementation(libs.kafka.avro.serializer)
 
-    implementation("no.nav.security:token-validation-spring:$tokenSupportVersion")
+    implementation(libs.poao.tilgang.client)
 
-    implementation("net.javacrumbs.shedlock:shedlock-spring:$shedlockVersion")
-    implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:$shedlockVersion")
+    implementation(libs.token.validation.spring)
+
+    implementation(libs.shedlock.spring)
+    implementation(libs.shedlock.provider.jdbc.template)
 
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "com.vaadin.external.google", module = "android-json")
@@ -71,20 +58,20 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("org.springframework.boot:spring-boot-resttestclient")
 
-    testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
-    testImplementation("io.kotest:kotest-assertions-json-jvm:$kotestVersion")
-    testImplementation("com.squareup.okhttp3:mockwebserver:$okhttp3Version")
+    testImplementation(libs.kotest.assertions.core)
+    testImplementation(libs.kotest.assertions.json)
+    testImplementation(libs.okhttp.mockwebserver)
 
-    testImplementation("org.testcontainers:testcontainers-postgresql")
-    testImplementation("org.testcontainers:testcontainers-kafka")
+    testImplementation(libs.testcontainers.postgresql)
+    testImplementation(libs.testcontainers.kafka)
 
-    testImplementation("io.mockk:mockk-jvm:$mockkVersion")
-    testImplementation("no.nav.security:mock-oauth2-server:$mockOauth2ServerVersion")
-    testImplementation("no.nav.amt.deltakelser.lib:testing:$amtLibVersion")
+    testImplementation(libs.mockk)
+    testImplementation(libs.mock.oauth2.server)
+    testImplementation(libs.amt.lib.testing)
 }
 
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(25)
     compilerOptions {
         freeCompilerArgs.addAll(
             "-Xjsr305=strict",
@@ -95,7 +82,7 @@ kotlin {
 }
 
 ktlint {
-    version = ktLintVersion
+    version = libs.versions.ktlint.cli.version.get()
 }
 
 tasks.register<GenerateAvroTask>("generateAvroJava") {
