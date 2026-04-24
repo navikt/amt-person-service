0dea8f8a1d8e43b127425e49de6b770bcccbf05f
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-04-24 13:28:08 +0200

    KtLint fixes

--- src/main/kotlin/no/nav/amt/person/service/person/dbo/PersonDbo.kt
+++ src/main/kotlin/no/nav/amt/person/service/person/dbo/PersonDbo.kt
@@ -4,16 +4,16 @@ import java.time.LocalDateTime
 import java.util.UUID
 
 data class PersonDbo(
-	val id: UUID,
-	val personident: String,
-	val erFalskIdentitet: Boolean,
-	val fornavn: String,
-	val mellomnavn: String?,
-	val etternavn: String,
-	val createdAt: LocalDateTime = LocalDateTime.now(),
-	val modifiedAt: LocalDateTime = LocalDateTime.now(),
+    val id: UUID,
+    val personident: String,
+    val erFalskIdentitet: Boolean,
+    val fornavn: String,
+    val mellomnavn: String?,
+    val etternavn: String,
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    val modifiedAt: LocalDateTime = LocalDateTime.now(),
 ) {
-	companion object {
-		const val UNKNOWN_NAME = "Ukjent"
-	}
+    companion object {
+        const val UNKNOWN_NAME = "Ukjent"
+    }
 }
