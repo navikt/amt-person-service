f1570963b4d49056fae9516eeb1945db9729b8d5
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-04-24 13:22:43 +0200

    Reformatert kode

--- src/main/kotlin/no/nav/amt/person/service/clients/pdl/PdlQueriesAttributeExtensions.kt
+++ src/main/kotlin/no/nav/amt/person/service/clients/pdl/PdlQueriesAttributeExtensions.kt
@@ -23,15 +23,14 @@ fun PdlQueries.HentPerson.ResponseData.toPdlBruker(postnummerTilPoststedFunc: (S
         etternavn = navn.etternavn,
         telefonnummer = hentPerson.telefonnummer.toTelefonnummer(),
         adressebeskyttelseGradering = hentPerson.adressebeskyttelse.toDiskresjonskode(),
-        identer =
-            hentIdenter.identer.map
-                {
-                    Personident(
-                        it.ident,
-                        it.historisk,
-                        IdentType.valueOf(it.gruppe),
-                    )
-                },
+        identer = hentIdenter.identer.map
+            {
+                Personident(
+                    it.ident,
+                    it.historisk,
+                    IdentType.valueOf(it.gruppe),
+                )
+            },
         adresse = hentPerson.toAdresse(postnummerTilPoststedFunc),
     )
 }
@@ -68,12 +67,11 @@ private fun PdlQueries.HentPerson.HentPerson.toAdresse(postnummerTilPoststedFunc
 
     if (poststeder.isEmpty()) return null
 
-    val adresse =
-        Adresse(
-            bostedsadresse = bostedsadresseFraPdl?.toBostedsadresse(poststeder),
-            oppholdsadresse = oppholdsadresseFraPdl?.toOppholdsadresse(poststeder),
-            kontaktadresse = kontaktadresseFraPdl?.toKontaktadresse(poststeder),
-        )
+    val adresse = Adresse(
+        bostedsadresse = bostedsadresseFraPdl?.toBostedsadresse(poststeder),
+        oppholdsadresse = oppholdsadresseFraPdl?.toOppholdsadresse(poststeder),
+        kontaktadresse = kontaktadresseFraPdl?.toKontaktadresse(poststeder),
+    )
 
     if (adresse.bostedsadresse == null && adresse.oppholdsadresse == null && adresse.kontaktadresse == null) {
         return null
@@ -124,12 +122,11 @@ private fun PdlQueries.Attribute.Kontaktadresse.toKontaktadresse(poststeder: Lis
     if (vegadresse == null && postboksadresse == null) {
         return null
     }
-    val kontaktadresse =
-        Kontaktadresse(
-            coAdressenavn = coAdressenavn,
-            vegadresse = vegadresse?.toVegadresse(poststeder),
-            postboksadresse = postboksadresse?.toPostboksadresse(poststeder),
-        )
+    val kontaktadresse = Kontaktadresse(
+        coAdressenavn = coAdressenavn,
+        vegadresse = vegadresse?.toVegadresse(poststeder),
+        postboksadresse = postboksadresse?.toPostboksadresse(poststeder),
+    )
     if (kontaktadresse.vegadresse == null && kontaktadresse.postboksadresse == null) {
         return null
     }
