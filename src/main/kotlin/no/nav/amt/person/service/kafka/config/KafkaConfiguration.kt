fecb01a70f9d08304d1241c9ea7c914306843415
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-08-09 17:40:08 +0200

    Flyttet params nærmere usage

--- src/main/kotlin/no/nav/amt/person/service/kafka/config/KafkaConfiguration.kt
+++ src/main/kotlin/no/nav/amt/person/service/kafka/config/KafkaConfiguration.kt
@@ -5,6 +5,7 @@ import no.nav.common.kafka.consumer.KafkaConsumerClient
 import no.nav.common.kafka.consumer.feilhandtering.KafkaConsumerRecordProcessor
 import no.nav.common.kafka.consumer.feilhandtering.util.KafkaConsumerRecordProcessorBuilder
 import no.nav.common.kafka.consumer.util.KafkaConsumerClientBuilder
+import no.nav.common.kafka.spring.PostgresJdbcTemplateConsumerRepository
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -12,22 +13,22 @@ import org.springframework.jdbc.core.JdbcTemplate
 
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnProperty("kafka.enabled", havingValue = "true", matchIfMissing = true)
-class KafkaConfiguration(
-    private val kafkaProperties: KafkaProperties,
-    private val jdbcTemplate: JdbcTemplate,
-) {
+class KafkaConfiguration {
     @Bean
-    fun kafkaConsumerClient(topicConfigs: List<KafkaConsumerClientBuilder.TopicConfig<*, *>>): KafkaConsumerClient =
-        KafkaConsumerClientBuilder
-            .builder()
-            .withProperties(kafkaProperties.consumer())
-            .withTopicConfigs(topicConfigs)
-            .build()
+    fun kafkaConsumerClient(
+        kafkaProperties: KafkaProperties,
+        topicConfigs: List<KafkaConsumerClientBuilder.TopicConfig<*, *>>,
+    ): KafkaConsumerClient = KafkaConsumerClientBuilder
+        .builder()
+        .withProperties(kafkaProperties.consumer())
+        .withTopicConfigs(topicConfigs)
+        .build()
 
     @Bean
     fun kafkaConsumerRecordProcessor(
         topicConfigs: List<KafkaConsumerClientBuilder.TopicConfig<*, *>>,
-        consumerRepository: no.nav.common.kafka.spring.PostgresJdbcTemplateConsumerRepository,
+        consumerRepository: PostgresJdbcTemplateConsumerRepository,
+        jdbcTemplate: JdbcTemplate,
     ): KafkaConsumerRecordProcessor = KafkaConsumerRecordProcessorBuilder
         .builder()
         .withLockProvider(JdbcTemplateLockProvider(jdbcTemplate))
