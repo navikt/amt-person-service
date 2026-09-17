538e1fedf636230f276a53bd584e555bb1e9d302
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-09-17 21:58:18 +0200

    komet-bau: Hopper midlertidig over personer som ikke finnes i PDL

--- src/main/kotlin/no/nav/amt/person/service/kafka/consumer/InnsatsgruppeConsumer.kt
+++ src/main/kotlin/no/nav/amt/person/service/kafka/consumer/InnsatsgruppeConsumer.kt
@@ -22,10 +22,18 @@ class InnsatsgruppeConsumer(
     fun ingest(value: String) {
         val siste14aVedtak = objectMapper.readValue<Siste14aVedtak>(value)
 
-        val gjeldendeIdent = pdlClient
-            .hentIdenter(siste14aVedtak.aktorId)
-            .finnGjeldendeIdent()
-            .getOrThrow()
+        val gjeldendeIdent = try {
+            pdlClient
+                .hentIdenter(siste14aVedtak.aktorId)
+                .finnGjeldendeIdent()
+                .getOrThrow()
+        } catch (e: RuntimeException) {
+            if (e.message?.contains("Fant ikke person") == true) {
+                log.warn("Fant ikke person i PDL, hopper over Kafka-melding")
+                return
+            }
+            throw e
+        }
 
         val brukerId = navBrukerRepository.finnBrukerId(gjeldendeIdent.ident)
 
